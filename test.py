30ee68f5216a89cc37305417a9635cd2430f2201
EC2 Default User <ec2-user@ip-172-31-27-68.us-east-2.compute.internal>
2020-04-13 04:05:28 +0000

    create test.py

---
+++ test.py
@@ -0,0 +1,22 @@
+def f(x, y): 
+    z = x*y 
+    a = x + y
+    b = "あいう"
+    return z,a,b
+
+
+m = 10 
+n = 5 
+k = f(m, n)
+print(len(k))
+print(type (k))
+
+print(k, end='')
+
+list_data = [0,1,2,3]
+list_data.remove(2)
+print(list_data)
+
+print(type(list_data))
+
+print("git")
